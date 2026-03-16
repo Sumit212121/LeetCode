cb311bf6f7e33cad33d8574ee238b4f4e94d413e
Sumit212121 <sumitbarnwal844@gmail.com>
2026-03-16 16:18:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (90.32%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        a= nums*2
+        return a
+        
